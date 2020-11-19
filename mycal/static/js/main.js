971daa9f78c890f47d9aa849ade12b0f45af82ca
Marlon Howard <mjhow4@gmail.com>
2020-11-19 14:22:05 -0500

    minor updates

--- mycal/static/js/main.js
+++ mycal/static/js/main.js
@@ -1,15 +1,22 @@
 console.log("loaded")
 
-
-let btnShow = document.querySelector('button');
-let result = document.querySelector('h1');
-
-btnShow.addEventListener('click', () => {
-    let checkbox = document.querySelector('input[type="checkbox"]: checked');
-    result.innerText = checkbox.value;
-
-}); 
-
-
 // let attendanceBoxes = document.querySelectorAll('checkbox')
 // console.log(attendanceBoxes)
+
+
+function getValue() {
+    var checks = document.getElementsByClassName('checks');
+    var str = ''
+
+    for (i = 0; i < 3; i++) {
+        if (checks[i].checked == true) {
+            str += checks[i].value;
+            console.log(str)
+        }
+    
+      
+    }
+    alert(str)
+   
+}
+
