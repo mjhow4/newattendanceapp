a29cd72390b50b13853c2454d732c1d46eb4df80
Marlon Howard <mjhow4@gmail.com>
2020-11-19 12:17:19 -0500

    fixed pink letter issue

--- mycal/static/js/main.js
+++ mycal/static/js/main.js
@@ -1,15 +1,15 @@
 console.log("loaded")
 
 
-// let btnShow = document.querySelector('button');
-// let result = document.querySelector('h1');
+let btnShow = document.querySelector('button');
+let result = document.querySelector('h1');
 
-// btnShow.addEventListener('click', () => {
-//     let checkbox = document.querySelector('input[type="checkbox"]: checked');
-//     result.innerText = checkbox.value;
+btnShow.addEventListener('click', () => {
+    let checkbox = document.querySelector('input[type="checkbox"]: checked');
+    result.innerText = checkbox.value;
 
-// }); 
+}); 
 
 
-let attendanceBoxes = document.querySelectorAll('checkbox')
-console.log(attendanceBoxes)
+// let attendanceBoxes = document.querySelectorAll('checkbox')
+// console.log(attendanceBoxes)
