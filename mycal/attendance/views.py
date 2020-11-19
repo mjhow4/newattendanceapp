f5f231febc39ffd39f49ee348d71dd693e084b0e
Marlon Howard <mjhow4@gmail.com>
2020-11-19 12:03:10 -0500

    updates from class

--- mycal/attendance/views.py
+++ mycal/attendance/views.py
@@ -157,4 +157,10 @@ def delete_case(request, pk):
                   {"case": case})   
 
 
+def updateattendance(request):
+    case = get_object_or_404(Case, pk=pk)
+    case.update()
+    
+
+
  
--- mycal/static/css/main.js
+++
@@ -1,8 +0,0 @@
-let btnShow = document.querySelector('button');
-let result = document.querySelector('h1');
-
-btnShow.addEventListener('click', () => {
-    let checkbox = document.querySelector('input[type="checkbox"]: checked');
-    result.innerText = checkbox.value;
-
-}); 
